b0e700dfd42bcd04340c438b6523c969bf0d0155
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-02-04 10:42:48 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+  const paragraph = document.querySelector("#text");
+  paragraph.innerHTML = "This is really cool!";
+});
